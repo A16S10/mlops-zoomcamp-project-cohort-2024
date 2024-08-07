0c9847f84c1d79e93bc4975c64076f809ac89cd7
Nilesh Arte <nilart@gmail.com>
2024-08-07 20:39:49 +0200

    python script for batch deploy model

---
+++ batch.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import pickle
+import numpy as np
+
+input_file = f'employee_data.csv'
+output_file = f'output/predicted_data.csv'
+
+
+df = pd.read_csv(input_file)
+# Load the model from the file
+with open('./models/lin_reg.bin', 'rb') as file:
+    dv, lr = pickle.load(file)
+
+categorical = ['Gender', 'Position']
+numerical = ['Experience (Years)']
+
+dicts = df[categorical + numerical].to_dict(orient='records')
+
+X_val = dv.transform(dicts)
+y_pred = lr.predict(X_val)
+
+
+# Calculate the standard deviation of the predicted durations
+std_dev = np.std(y_pred)
+print("Standard Deviation of Predicted Durations:", std_dev)
+
+print('predicted mean duration:', y_pred.mean())
+
+
+df_result = pd.DataFrame()
+df_result['predicted_salary'] = y_pred
+print (df_result)
